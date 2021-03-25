954ed62c1d1f315d1876d1daa0d4952a3a6b7610
Elijah Xiong <exiong@icstars.org>
2021-03-25 11:33:40 -0500

    adventure text!

---
+++ Adventure text backup 2.0/main.js
@@ -0,0 +1,77 @@
+function startGame() {
+  let userName = prompt("What is your name?");
+  let userWeapon = prompt("Choose your weapon: Dagger, Sword, or Scythe?");
+  let userShoes = prompt("Choose your shoes: Jordans, Crocs, or Boots");
+
+  if (userName === "" && userWeapon === "" && userShoes === "") {
+    alert("You didn't enter anything");
+  } else if (userName != null && userWeapon != null && userShoes != null) {
+    alert(
+      "Wakeup! " +
+        userName +
+        " Wakeup!!!! Someone stole your collection of " +
+        userShoes +
+        " ... In a rush you grab the " +
+        userWeapon +
+        " and run out your room"
+    );
+  }
+}
+function promptFight() {
+  let sceneOne = prompt(
+    "As you run downstairs you see a masked thief pillaging your house a:throw b: charge (choose a letter)"
+  );
+  if (sceneOne === "") {
+    alert("You didn't do anything and fainted");
+  } else if (sceneOne) {
+    switch (sceneOne) {
+      case "a":
+        alert(
+          "You threw your weapon at the thief and hit their right shoulder.... The police came and you went to sleep."
+        );
+        break;
+      case "b":
+        alert(
+          "You charged and noticed that the thief was a stunning girl.... So you let her go and went to bed"
+        );
+        break;
+    }
+  }
+}
+function promptDayTwo() {
+  let userChoice = prompt(
+    "What would you like to do today: Casino or Black Market"
+  );
+  if (userChoice === "") {
+    alert("You didn't choose anything");
+  } else if (userChoice != null) {
+    alert("You have chosen " + userChoice);
+  }
+  if (userChoice === "Casino") {
+    alert("You went to the casino and played slots!");
+    for (let i = 0; i < 10; i++) {
+      if (i === 5) {
+        alert("You won in slots!");
+        continue;
+      }
+    }
+  }
+
+  if (userChoice === "Black Market") {
+    alert(
+      "You went to the Black Market and saw a muscular man standing at the door ."
+    );
+
+    let secretCode = prompt("What is the secret code?");
+    while (secretCode === "chicken") {
+      alert('Guard says:"You may enter........" THE END');
+    }
+    while (secretCode != "chicken") {
+      alert("*SLAP *KICK *PUNCH You have been kidnapped......... THE END");
+    }
+  }
+}
+
+startGame();
+promptFight();
+promptDayTwo();
